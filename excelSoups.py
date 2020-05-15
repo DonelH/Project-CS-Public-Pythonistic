3799ff8e58d83943f2c5a79825ba670610d06b44
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-05-14 18:48:37 -0700

    Update excelSoups.py
    Added USA, should be final version

--- excelSoups.py
+++ excelSoups.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
-import mexicoSoup
 import canadaSoup
+import mexicoSoup
+import usaSoup
 
 # Convert list from scraper to xlsx
+def canToXLSX():
+    canadaProvinces = canadaSoup.scrapeCanada()
+    pd.DataFrame(canadaProvinces).to_excel('canadaSoup.xlsx')
+    
 def mexToXLSX():
     mexicoStates = mexicoSoup.scrapeMexico()
     pd.DataFrame(mexicoStates).to_excel('mexicoSoup.xlsx')
 
-def canToXLSX():
-    canadaStates = canadaSoup.scrapeCanada()
-    pd.DataFrame(canadaStates).to_excel('canadaSoup.xlsx')
+def usaToXLSX():
+    usaStates = usaSoup.scrapeUSA()
+    pd.DataFrame(usaStates).to_excel('usaSoup.xlsx')
     
 if __name__ == "__main__":
-    mexToXLSX()
     canToXLSX()
+    mexToXLSX()
+    usaToXLSX()
+
