9d88a79120d16c4cd0427b1960563d4b50f091ee
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-05-14 17:49:34 -0700

    Update canadaSoup.py
    Final Version of Canada Soup

--- canadaSoup.py
+++ canadaSoup.py
@@ -1,12 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
 import re
 
 url_data = 'https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html?topic=tilelink'
 page = requests.get(url_data)
-canadaData = open("canadaData.csv", "w")
-canadaData.close()
 canada_list = []
 
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,24 +15,14 @@ def scrapeCanada():
     for prov_elem in prov_elems:
         td_list = prov_elem.find_all('td')
         if(td_list):
-            prov_name = re.sub('<|t|d|>|/', '', str(td_list[0]))
-            prov_cases = re.sub('<|t|d|>|/', '', str(td_list[1]))
-            prov_deaths = re.sub('<|t|d|>|/', '', str(td_list[3]))
-            canada_list.append([prov_name, prov_cases, prov_deaths])
-    print(canada_list)
-    return canada_list  
-    canadaToCSV()
-    
-def canadaToCSV():
-
-    with open("canadaData.csv", 'w', newline='') as fout:
-        covid_writer = csv.writer(fout)
-        for each in canada_list:
-            covid_writer.writerow([each[0], each[1], each[2]])
-    canadaData.close()
+            prov_name = str(td_list[0])
+            prov_name = prov_name[4:len(prov_name) - 5]
+            prov_cases = re.sub('<|t|d|>|/|/|,', '', str(td_list[1]))
+            prov_deaths = re.sub('<|t|d|>|/|,', '', str(td_list[3]))
+            canada_list.append([prov_name, int(prov_cases), int(prov_deaths)])
+    return canada_list
 
 if __name__ == '__main__':
-
     scrapeCanada()
                    
 
