207a29dd5169ee1b69b4f8c26e94155ea0787ce9
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-05-14 17:51:47 -0700

    Update usaSoup.py
    USA Soup transitioning to wiki source

--- usaSoup.py
+++ usaSoup.py
@@ -1,21 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-#import lxml.html as lh
-import pandas as pd
+import re
+import csv
 
-URL = 'https://www.theguardian.com/world/ng-interactive/2020/apr/16/coronavirus-map-of-the-us-latest-cases-state-by-state'
+URL = 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States'
 page = requests.get(URL)
+mexicoData = open("usaData.csv", 'w')
+mexicoData.close()
+state_list = []
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id = "co-table-container")
-job_elems = results.find_all('tbody', class_= "co-tbody")
-print(job_elems)
+results = soup.find(id = 'mw-content-text')
+results_table = results.find('table', class_ = 'wikitable')
 
 state_list = []
-state_elems = results.find_all('td')
+state_elems = results_table.find_all('tr')
 for state_elem in state_elems:
-    print(state_elem.text)
-    state_list.append(state_elem.text)
+    if (state_elem.find('a') and state_elem.find('a').has_attr('title')
+        and not(state_elem.has_attr('style'))):
+        if(state_elem != ''):
+            state_title = state_elem.find('a').text
+            td_list = state_elem.find_all('td')
+            state_cases = re.sub('<|t|d|>|/|,', '', str(td_list[0]))
+            state_cases = state_cases.rstrip('\n')
+            state_deaths = re.sub('<|t|d|>|/|,', '', str(td_list[1]))
+            state_deaths = state_deaths.rstrip('\n')
+            print(state_elem)
+            state_list.append([state_title, state_cases, state_deaths])
 
 print(state_list)
 
