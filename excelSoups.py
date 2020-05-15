5a0a181a49548bdd1ac8857328af561c7ba30399
Eldon Hayes <eldon.hayes.eh@gmail.com>
2020-05-14 17:50:19 -0700

    Update excelSoups.py
    Added Canada to XLSX conversion

--- excelSoups.py
+++ excelSoups.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 import mexicoSoup
+import canadaSoup
 
 # Convert list from scraper to xlsx
-def toXLSX():
+def mexToXLSX():
     mexicoStates = mexicoSoup.scrapeMexico()
-    print(mexicoStates)
     pd.DataFrame(mexicoStates).to_excel('mexicoSoup.xlsx')
+
+def canToXLSX():
+    canadaStates = canadaSoup.scrapeCanada()
+    pd.DataFrame(canadaStates).to_excel('canadaSoup.xlsx')
     
 if __name__ == "__main__":
-    toXLSX()
+    mexToXLSX()
+    canToXLSX()
